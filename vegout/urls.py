6741ddb8c439a0076d8f591a6093e38f5d92deae
Laurent Peuch <cortex@worlddomination.be>
2014-11-08 16:59:22 +0100

    [enh] configure project to display home.haml

--- vegout/urls.py
+++ vegout/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vegout.views.home', name='home'),
+    url(r'^', include('restaurant.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
