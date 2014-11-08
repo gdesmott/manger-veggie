1c140309e2f5d6b876c63f0cbcc1b91d59ea47e9
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-11-08 22:21:36 +0100

    move create_restaurant as a class method

--- restaurant/management/commands/populate.py
+++ restaurant/management/commands/populate.py
@@ -3,35 +3,17 @@ from django.core.management.base import BaseCommand
 #from optparse import make_option
 
 from restaurant.models import Restaurant
-from geopy.geocoders import Nominatim
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
     )
 
-    def create_restaurant(self, name, address, website=None, phone=None, mail=None, contact=None, status=None, vg_contact=None):
-        if website is not None and not website.startswith('http'):
-            website = 'http://%s' % website
-
-        restaurant = Restaurant.objects.create(name=name, address=address, website=website,
-                phone=phone, mail=mail, contact=contact, status=status, vg_contact=vg_contact)
-        print "added", name
-
-        geolocator = Nominatim()
-        location = geolocator.geocode(address)
-        if location is not None:
-            restaurant.lat = location.latitude
-            restaurant.lon = location.longitude
-            restaurant.save()
-        else:
-            print "Unknown address", address
-
     def handle(self, *args, **options):
         Restaurant.objects.all().delete()
 
-        self.create_restaurant("Exki", "12, Chaussée D'Ixelles, 1050 Ixelles", 'www.exki.be', '02/502.72.77', status='2ème vague')
-        self.create_restaurant("Ellis Gourmet Burger", "Place Sainte-Catherine, 4 - 1000 Bruxelles", "http://www.ellisgourmetburger.com/nl/", "02/514.23.14", status="OK (autocollant)", vg_contact="Lisa & Sophie - 28/06")
-        self.create_restaurant("Den Teepot", "66, Rue des Chartreux 1000 Bruxelles", "http://www.bioshop.be/winkels/brussel.html", "02/511.94.02", status="OK (autocollant)", vg_contact="Lisa & Sophie - 28/06")
-        self.create_restaurant("Toukoul", "34, Rue de Laeken 1000 Bruxelles ", "http://www.toukoul.be", "02/223.73.77", "info@toukoul.be", status="repasser à 13h", vg_contact="Lisa & Sophie - 28/06")
-        self.create_restaurant("Mr Falafel", "53, Boulevard Lemonnier - 1000 Bruxelles", None, "0493/34.64.12", None, status="OK (autocollant)", vg_contact="Lisa & Sophie - 28/06")
-        self.create_restaurant("Le Dolma", "329, Chaussée d'Ixelles - 1050 Ixelles", "www.dolma.be", "02/649.89.81", "info@dolma.be", status="OK (autocollant)", vg_contact="Lisa & Sophie H & Isabelle - 05/07")
+        Restaurant.create("Exki", "12, Chaussée D'Ixelles, 1050 Ixelles", 'www.exki.be', '02/502.72.77', status='2ème vague')
+        Restaurant.create("Ellis Gourmet Burger", "Place Sainte-Catherine, 4 - 1000 Bruxelles", "http://www.ellisgourmetburger.com/nl/", "02/514.23.14", status="OK (autocollant)", vg_contact="Lisa & Sophie - 28/06")
+        Restaurant.create("Den Teepot", "66, Rue des Chartreux 1000 Bruxelles", "http://www.bioshop.be/winkels/brussel.html", "02/511.94.02", status="OK (autocollant)", vg_contact="Lisa & Sophie - 28/06")
+        Restaurant.create("Toukoul", "34, Rue de Laeken 1000 Bruxelles ", "http://www.toukoul.be", "02/223.73.77", "info@toukoul.be", status="repasser à 13h", vg_contact="Lisa & Sophie - 28/06")
+        Restaurant.create("Mr Falafel", "53, Boulevard Lemonnier - 1000 Bruxelles", None, "0493/34.64.12", None, status="OK (autocollant)", vg_contact="Lisa & Sophie - 28/06")
+        Restaurant.create("Le Dolma", "329, Chaussée d'Ixelles - 1050 Ixelles", "www.dolma.be", "02/649.89.81", "info@dolma.be", status="OK (autocollant)", vg_contact="Lisa & Sophie H & Isabelle - 05/07")
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from geopy.geocoders import Nominatim
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
@@ -15,6 +16,24 @@ class Restaurant(models.Model):
     contact = models.CharField(max_length=255, null=True, blank=True)
     status = models.CharField(max_length=255, null=True, blank=True)
     vg_contact = models.CharField(max_length=255, null=True, blank=True)
+
+    @classmethod
+    def create(cls, name, address, website=None, phone=None, mail=None, contact=None, status=None, vg_contact=None):
+        if website is not None and not website.startswith('http'):
+            website = 'http://%s' % website
+
+        restaurant = cls.objects.create(name=name, address=address, website=website,
+                phone=phone, mail=mail, contact=contact, status=status, vg_contact=vg_contact)
+        print "added", name
+
+        geolocator = Nominatim()
+        location = geolocator.geocode(address)
+        if location is not None:
+            restaurant.lat = location.latitude
+            restaurant.lon = location.longitude
+            restaurant.save()
+        else:
+            print "Unknown address", address
 
     def __unicode__(self):
         return self.name
