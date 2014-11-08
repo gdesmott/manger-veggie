ab2fe053fa744e7295deded224f0b910581a7980
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-11-08 16:56:10 +0100

    models: add Restaurant

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from restaurant.models import Restaurant
 
-# Register your models here.
+admin.site.register(Restaurant)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    website = models.URLField(null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    mail = models.EmailField(null=True, blank=True)
+
+    # admin
+    contact = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=255, null=True, blank=True)
+    vg_contact = models.CharField(max_length=255, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
