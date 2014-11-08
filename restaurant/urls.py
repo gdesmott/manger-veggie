b7ae7a355d470ff68b4f34419ad9107456b729c2
Laurent Peuch <cortex@worlddomination.be>
2014-11-08 19:18:55 +0100

    [enh] add a detail page per restaurant

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from .models import Restaurant
 
 
 urlpatterns = patterns('restaurant.views',
     url(r'^$', TemplateView.as_view(template_name="home.haml"), name='home'),
     url(r'^restaurants.json$', 'restaurants_json', name='home'),
+    url(r'^restaurant/(?P<pk>\d+)/$', DetailView.as_view(model=Restaurant, template_name="restaurant/restaurant_detail.haml"), name='home'),
 )
