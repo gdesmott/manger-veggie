fafc59f73ec04ffc74987d53b504a824324f963f
Laurent Peuch <cortex@worlddomination.be>
2014-11-19 13:20:32 +0100

    [mod] split the way the json is created to include methods in the futur

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,4 +5,12 @@ from .models import Restaurant
 
 
 def restaurants_json(request):
-    return HttpResponse(json.dumps(list(Restaurant.objects.filter(lat__isnull=False, lon__isnull=False, checked__exact=True).values("lat", "lon", "name", "address", "website", "phone", "id"))))
+    return HttpResponse(json.dumps([{
+        "lat": x.lat,
+        "lon": x.lon,
+        "name": x.name,
+        "address": x.address,
+        "website": x.website,
+        "phone": x.phone,
+        "id": x.id,
+    } for x in Restaurant.objects.filter(lat__isnull=False, lon__isnull=False, checked__exact=True)]))
