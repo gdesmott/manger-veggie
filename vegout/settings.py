c6eae6bfc46cdca62855466f319da23144e68c2a
Laurent Peuch <cortex@worlddomination.be>
2014-11-19 15:22:27 +0100

    [enh] add django_pdb to INSTALLED_APPS

--- vegout/settings.py
+++ vegout/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'django_pdb',
     'restaurant',
     'bootstrap3',
     'leaflet',
