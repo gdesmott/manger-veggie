f91511cf70eda5262efde180ddce00b8313dbf81
Laurent Peuch <cortex@worlddomination.be>
2015-07-25 11:26:42 +0200

    [enh] do the resto importation in a transaction

--- restaurant/management/commands/import-vegoresto.py
+++ restaurant/management/commands/import-vegoresto.py
@@ -5,6 +5,8 @@ from BeautifulSoup import BeautifulSoup
 import urllib2
 from dateutil.parser import parse
 
+from django.db import transaction
+
 source_url = 'http://vegoresto.fr/restos-fichier-xml/'
 
 VG_TAGS = {
@@ -36,30 +38,31 @@ class Command(BaseCommand):
         xml_content = s.read()
         soup = BeautifulSoup(xml_content)
 
-        for resto_data in soup.root.findAll('item'):
-            vegoresto_id = int(resto_data.id.text)
-            resto_set = Restaurant.objects.filter(vegoresto_id=vegoresto_id)
-            print 'importing {0}'.format(resto_data.titre.text.encode('utf-8'))
+        with transaction.atomic():
+            for resto_data in soup.root.findAll('item'):
+                vegoresto_id = int(resto_data.id.text)
+                resto_set = Restaurant.objects.filter(vegoresto_id=vegoresto_id)
+                print 'importing {0}'.format(resto_data.titre.text.encode('utf-8'))
 
-            if resto_set.exists():
-                resto = resto_set[0]
-            else:
-                resto = Restaurant.create(vegoresto_id=vegoresto_id,
-                                          name=resto_data.titre.text,
-                                          address=resto_data.adresse.text)
+                if resto_set.exists():
+                    resto = resto_set[0]
+                else:
+                    resto = Restaurant.create(vegoresto_id=vegoresto_id,
+                                              name=resto_data.titre.text,
+                                              address=resto_data.adresse.text)
 
-            resto.review = resto_data.vegetik_review.text
-            resto.approved_date = parse(resto_data.vegetik_approved_date.text)
-            resto.lat = float(resto_data.lat.text)
-            resto.lon = float(resto_data.lon.text)
-            resto.website = resto_data.link.text
-            resto.description = resto_data.description.text
-            resto.phone = resto_data.tel_fixe.text
-            resto.mail = resto_data.mel_public.text
-            resto.main_image = resto_data.image.text
-            resto.country_code = resto_data.pays.text.upper()
-            tags = parse_vg_tags(resto_data.categories_culinaires.text)
-            if tags:
-                resto.tags.add(*tags)
+                resto.review = resto_data.vegetik_review.text
+                resto.approved_date = parse(resto_data.vegetik_approved_date.text)
+                resto.lat = float(resto_data.lat.text)
+                resto.lon = float(resto_data.lon.text)
+                resto.website = resto_data.link.text
+                resto.description = resto_data.description.text
+                resto.phone = resto_data.tel_fixe.text
+                resto.mail = resto_data.mel_public.text
+                resto.main_image = resto_data.image.text
+                resto.country_code = resto_data.pays.text.upper()
+                tags = parse_vg_tags(resto_data.categories_culinaires.text)
+                if tags:
+                    resto.tags.add(*tags)
 
-            resto.save()
+                resto.save()
