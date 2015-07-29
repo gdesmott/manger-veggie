b4aad4779a8feedbf3643dae6691f54b04da0f06
Laurent Peuch <cortex@worlddomination.be>
2015-07-29 12:12:41 +0200

    [enh] import all vegoresto tags and display them in another manner

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,6 +2,7 @@ import json
 from django.http import HttpResponse
 
 from .models import Restaurant
+from .utils import VG_TAGS
 
 
 def restaurants_json(request):
@@ -15,7 +16,8 @@ def restaurants_json(request):
         "id": x.id,
         "national_phone_number": x.get_national_phone_number(),
         "international_phone_number": x.get_international_phone_number(),
-        "tags": [tag["name"] for tag in x.tags.all().values("name")],
+        "tags": [tag["name"] for tag in x.tags.all().values("name") if tag["name"] in VG_TAGS.values()],
+        "other_tags": [tag["name"] for tag in x.tags.all().values("name") if tag["name"] not in VG_TAGS.values()],
     } for x in Restaurant.objects.filter(lat__isnull=False, lon__isnull=False, active=True)], indent=4))
 
     # optimisation note: .prefetch_related("tags") makes request slower here
